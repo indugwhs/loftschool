3161b1d2d7910b5a64d3ebd47263b8d8e25e1afa
indugwhs <sagalakova3105@mail.ru>
2021-05-15 04:02:56 +0700

    homework 5

--- app/src/main/java/com/indugwhs/loftschool/screens/balance/BalanceFragment.java
+++ app/src/main/java/com/indugwhs/loftschool/screens/balance/BalanceFragment.java
@@ -1,18 +1,38 @@
 package com.indugwhs.loftschool.screens.balance;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.indugwhs.loftschool.R;
+import com.indugwhs.loftschool.screens.LoftApp;
+
+import java.util.Random;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class BalanceFragment extends Fragment {
 
+    private TextView balance;
+    private TextView expense;
+    private TextView income;
+    private BalanceView balanceView;
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Nullable
     @Override
@@ -24,6 +44,30 @@ public class BalanceFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        balance = view.findViewById(R.id.txtBalanceFinanceValue);
+        expense = view.findViewById(R.id.txtBalanceCostsValue);
+        income = view.findViewById(R.id.txtBalanceIncomeValue);
+        BalanceView balanceView = view.findViewById(R.id.balanceView);
+        swipeRefreshLayout = view.findViewById(R.id.swipe_refresh_balance);
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            loadBalance();
+            swipeRefreshLayout.setRefreshing(false);
+        });
+        loadBalance();
+    }
 
+
+    public void loadBalance() {
+        String authToken = getActivity().getSharedPreferences(getString(R.string.app_name), 0).getString(LoftApp.AUTH_KEY, "");
+        Disposable disposable = ((LoftApp) getActivity().getApplication()).getMoneyApi().getBalance(authToken)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(balanceResponse -> {
+                    String balanceString = String.valueOf(balanceResponse.getTotalIncome() - balanceResponse.getTotalExpenses());
+                    balance.setText(balanceString + "₽");
+                    income.setText(String.valueOf(balanceResponse.getTotalIncome()) + "₽");
+                    expense.setText(String.valueOf(balanceResponse.getTotalExpenses()) + "₽");
+                }, throwable -> Toast.makeText(getActivity().getApplicationContext(), throwable.getLocalizedMessage(), Toast.LENGTH_LONG).show());
+        compositeDisposable.add(disposable);
     }
 }
--- app/src/main/java/com/indugwhs/loftschool/screens/balance/BalanceView.java
+++ app/src/main/java/com/indugwhs/loftschool/screens/balance/BalanceView.java
@@ -2,30 +2,53 @@ package com.indugwhs.loftschool.screens.balance;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+import com.indugwhs.loftschool.R;
 
 public class BalanceView extends View {
 
-    private float expenses;
-    private float incomes;
+    private float expenses = 5400;
+    private float incomes = 7400;
+
+    private Paint expensePaint= new Paint();
+    private Paint incomesPaint= new Paint();
 
     public BalanceView(Context context) {
         super(context);
+        init();
     }
 
     public BalanceView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public BalanceView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     public BalanceView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init(){
+        expensePaint.setColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+        incomesPaint.setColor(ContextCompat.getColor(getContext(), R.color.green));
+    }
+
+    public void update(float expenses, float incomes){
+        this.incomes = incomes;
+        this.expenses = expenses;
+
+        invalidate();
     }
 
     @Override
@@ -33,7 +56,22 @@ public class BalanceView extends View {
         super.onDraw(canvas);
 
         float total = expenses + incomes;
+        float expenseAngle = 360f * expenses / total;
+        float incomesAngle = 360f * incomes / total;
 
+        int space = 15;
+        int size = Math.min(getWidth(), getHeight()) - space * 2;
+        int xMargin = (getWidth() - size) / 2;
+        int yMargin = (getHeight() - size) / 2;
 
+        canvas.drawArc(xMargin - space, yMargin,
+                getWidth() - xMargin - space,
+                getHeight() - yMargin,
+                180 - expenseAngle / 2, expenseAngle, true, expensePaint);
+
+        canvas.drawArc(xMargin + space, yMargin,
+                getWidth() - xMargin + space,
+                getHeight() - yMargin,
+                360 - incomesAngle / 2, incomesAngle, true, incomesPaint);
     }
 }
